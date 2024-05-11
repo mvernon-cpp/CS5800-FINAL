fdfb79450152176b7f209684454444d704dee3a4
fidelisprasetyo <fidelisprasetyo@gmail.com>
2024-05-10 20:11:19 -0700

    init

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,11 +1,25 @@
 public class Customer {
-    private final String name;
 
-    public Customer(String name) {
+    private final String name;
+    private final String address;
+    private final String county;
+    private final Diet
+
+    public Customer(String name, String address, String county) {
         this.name = name;
+        this.address = address;
+        this.county = county;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCounty() {
+        return county;
     }
 }
